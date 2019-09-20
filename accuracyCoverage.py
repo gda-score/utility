6ae9c9efce6a7bfcbafec1a311ce280b504364f4
Paul Francis <paul@francis.com>
2019-09-20 12:33:30 +0200

    initial populate

---
+++ accuracyCoverage.py
@@ -0,0 +1,24 @@
+import sys
+from gdascore.gdaUtilities import setupGdaAttackParameters
+from utility.gdaUtility import gdaUtility
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
+gdaUtilityObj=gdaUtility()
+paramsList = setupGdaAttackParameters(sys.argv)
+print(f" param list:")
+pp.pprint(paramsList)
+for param in paramsList:
+    print("Start next param")
+    if param['finished'] == True:
+        print("The following Utility measure has been previously completed:")
+        pp.pprint(param)
+        print(f"Results may be found at {param['resultsPath']}")
+        continue
+    res = gdaUtilityObj.distinctUidUtilityMeasureSingleAndDoubleColumn(param)
+    if res is None:
+        print("Unable to complete.")
+        pp.pprint(param)
+        continue
+    print("Finish up")
+    gdaUtilityObj.finishGdaUtility(param)
---
+++ pdoc.bat
@@ -0,0 +1,1 @@
+C:\Users\francis\AppData\Local\Programs\Python\Python36-32\python.exe C:\Users\francis\AppData\Local\Programs\Python\Python36-32\Scripts\pdoc --html --overwrite gdaUtility.py
